05beda42f4feb3e7fa0a62dde7a3e62ff48e8c22
Tyler Trout <tiller1010@gmail.com>
2018-10-08 12:13:22 -0400

    working on making message props that cycle with page number

--- src/App.js
+++ src/App.js
@@ -5,33 +5,34 @@ class Page extends Component{
   constructor(props){
     super();
     this.state={
-      alignment: this.pages[this.pageNumber]
+      currentPage: this.pages[this.pageNumber]
     }
     this.turnPage=this.turnPage.bind(this);
     this.turnPageBack=this.turnPageBack.bind(this);
   }
 
-  pages=['left','right'];
+  pages=['one','two'];
   pageNumber=0;
 
   turnPage(){
     ++this.pageNumber;
     this.setState({
-      alignment: this.pages[this.pageNumber]
+      currentPage: this.pages[this.pageNumber]
     });
   }
 
   turnPageBack(){
     --this.pageNumber;
     this.setState({
-      alignment: this.pages[this.pageNumber]
+      currentPage: this.pages[this.pageNumber]
     });
   }
 
   render(){
     return(
-      <div className={this.state.alignment}>
+      <div className={this.state.currentPage}>
         <button onClick={this.turnPageBack}>Previous Page</button>
+        <div className='frame'></div>
         {this.props.pageText}
         <button onClick={this.turnPage}>Next Page</button>
       </div>
@@ -40,9 +41,11 @@ class Page extends Component{
 }
 
 class App extends Component {
+  messages=['This is the first page','This is the second page'];
+  pageNumber=0;
   render() {
     return (
-      <Page pageText="This is some longer sample text"/>
+      <Page pageText={this.messages[0]}/>
     );
   }
 }
